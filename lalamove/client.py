8906735fb4b707bc06fcb93a10a2b9ff1002dea2
oche92 <yosephoche@gmail.com>
2021-09-29 14:44:44 +0800

    add gitignore file and ignore venv directory

---
+++ lalamove/client.py
@@ -0,0 +1,17 @@
+import requests
+from requests import Response
+
+
+class Client(object):
+    def __init__(self):
+        pass
+
+    @property
+    def headers(self):
+        pass
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
